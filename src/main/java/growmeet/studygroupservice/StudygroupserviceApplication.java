c99de870182a060b88f1a2e844a0a5be8a792026
sytTest <syt1128@naver.com>
2025-03-13 14:12:37 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/growmeet/studygroupservice/StudygroupserviceApplication.java
@@ -0,0 +1,13 @@
+package growmeet.studygroupservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudygroupserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudygroupserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/growmeet/studygroupservice/StudygroupserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package growmeet.studygroupservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudygroupserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

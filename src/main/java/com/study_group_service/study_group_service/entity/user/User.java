4ceb7c23fb768cec961d619c073c6746c273d3f3
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-07-07 20:39:07 +0900

    feat: 연속 출석일 필드 추가, 반영 메서드 추가

--- src/main/java/com/study_group_service/study_group_service/entity/user/User.java
+++ src/main/java/com/study_group_service/study_group_service/entity/user/User.java
@@ -1,9 +1,13 @@
 package com.study_group_service.study_group_service.entity.user;
 
 import com.study_group_service.study_group_service.enums.Role;
+import com.study_group_service.study_group_service.entity.study.StudyRoomMeeting;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -21,11 +25,31 @@ public class User {
     private String name;
     private String phone;
     private LocalDateTime created_at = LocalDateTime.now();
+    private Long consecutiveAttendance;
+    private Long roomCount;
+
+    private LocalDate lastAttendanceDate;
 
     @Enumerated(EnumType.ORDINAL)
     private Role role = Role.USER;
 
+    @ManyToMany(mappedBy = "participants")
+    private Set<StudyRoomMeeting> meetings = new HashSet<>();
+
     public void promoteToAdmin() {
         this.role = Role.ADMIN;
+    }
+
+    public void updateDays() {
+        this.consecutiveAttendance++;
+    }
+
+    public void checkAttendance(LocalDate today) {
+        if (lastAttendanceDate == null || !lastAttendanceDate.plusDays(1).equals(today)) {
+            consecutiveAttendance = 0L;
+        } else {
+            consecutiveAttendance++;
+        }
+        lastAttendanceDate = today;
     }
 }

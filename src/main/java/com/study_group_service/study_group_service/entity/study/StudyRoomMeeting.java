50ef574ab490ec9aa5ee91a0d45bb44c6fe00968
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-07-07 20:37:35 +0900

    feat: 스터디룸과 스터디룸의 일정관리를 연관관계 + 일정 제목 및 내용 추가 구현

--- src/main/java/com/study_group_service/study_group_service/entity/study/StudyRoomMeeting.java
+++ src/main/java/com/study_group_service/study_group_service/entity/study/StudyRoomMeeting.java
@@ -1,12 +1,24 @@
 package com.study_group_service.study_group_service.entity.study;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.study_group_service.study_group_service.entity.user.User;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "studyRoom"})
 @Entity
+@Table(
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"study_room_id"})
+    }
+)
 @Getter
+@Setter
 public class StudyRoomMeeting {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,4 +29,15 @@ public class StudyRoomMeeting {
     private StudyRoom studyRoom;
 
     private LocalDateTime meetingTime;
+
+    private String title;
+    private String duration;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+        name = "user_meeting",
+        joinColumns = @JoinColumn(name = "meeting_id"),
+        inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> participants = new HashSet<>();
 }

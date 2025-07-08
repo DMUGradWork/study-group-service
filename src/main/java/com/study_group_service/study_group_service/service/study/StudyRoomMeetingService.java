1f62f198b61cec1f16b325a62fa2a00b73515c0e
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-07-08 21:09:54 +0900

    feat: 스터디 일정 서비스 구현

--- src/main/java/com/study_group_service/study_group_service/service/study/StudyRoomMeetingService.java
+++ src/main/java/com/study_group_service/study_group_service/service/study/StudyRoomMeetingService.java
@@ -1,7 +1,7 @@
 package com.study_group_service.study_group_service.service.study;
 
 import com.study_group_service.study_group_service.entity.study.StudyRoomMeeting;
-
+ 
 public interface StudyRoomMeetingService {
     StudyRoomMeeting createMeeting(Long studyRoomId, String title, String duration, java.time.LocalDateTime meetingTime);
     StudyRoomMeeting getMeetingByRoomId(Long roomId);

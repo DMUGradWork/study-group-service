67dfebef410a5a8d46ff3102282b312b0cd2ef3b
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-07-08 21:06:42 +0900

    feat: 프론트 전송을 위한 @JsonProperty 어노테이션 추가

--- src/main/java/com/study_group_service/study_group_service/dto/chat/ChatRoomMessageDTO.java
+++ src/main/java/com/study_group_service/study_group_service/dto/chat/ChatRoomMessageDTO.java
@@ -1,17 +1,26 @@
 package com.study_group_service.study_group_service.dto.chat;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class ChatRoomMessageDTO {
+    @JsonProperty("chatRoomId")
     private Long chatRoomId;
+    @JsonProperty("userId")
     private Long userId;
+    @JsonProperty("sender")
     private String sender;
+    @JsonProperty("content")
     private String content;
+    @JsonProperty("sentAt")
     private LocalDateTime sentAt;
+    @JsonProperty("imageUrl")
+    private String imageUrl;
 }

f20317eaa48e182f4d26d3235067577f7a391db4
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-07-08 21:05:11 +0900

    feat: AppConfig 구현

---
+++ src/main/java/com/study_group_service/study_group_service/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.study_group_service.study_group_service.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
+    }
+}

6636a7f20548420c92efa2dcbe5d0e35022a5522
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-07-08 21:03:55 +0900

    feat: 이미지 정적 리소스 구현

---
+++ src/main/java/com/study_group_service/study_group_service/config/WebMvcConfig.java
@@ -0,0 +1,14 @@
+package com.study_group_service.study_group_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
+    }
+} 

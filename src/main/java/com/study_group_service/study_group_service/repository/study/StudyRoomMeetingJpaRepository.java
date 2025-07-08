44b88b77ddbd00ca09f4973992a8ac85e6bed049
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-07-08 21:09:42 +0900

    feat: 스터디 일정 리포지토리 구현

--- src/main/java/com/study_group_service/study_group_service/repository/study/StudyRoomMeetingJpaRepository.java
+++ src/main/java/com/study_group_service/study_group_service/repository/study/StudyRoomMeetingJpaRepository.java
@@ -2,6 +2,6 @@ package com.study_group_service.study_group_service.repository.study;
 
 import com.study_group_service.study_group_service.entity.study.StudyRoomMeeting;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+ 
 public interface StudyRoomMeetingJpaRepository extends JpaRepository<StudyRoomMeeting, Long> {
 } 

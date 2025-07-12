484e59664e1fbbfde5b2a785baa2288483a8d10a
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-07-12 21:29:26 +0900

    refactor: 에러메세지 전달 필드 수정

--- src/main/java/com/study_group_service/study_group_service/exception/study/StudyRoomNotFoundException.java
+++ src/main/java/com/study_group_service/study_group_service/exception/study/StudyRoomNotFoundException.java
@@ -1,7 +1,10 @@
 package com.study_group_service.study_group_service.exception.study;
 
+import static com.study_group_service.study_group_service.message.ErrorMessage.showNoStudyRoomMessage;
+
 public class StudyRoomNotFoundException extends RuntimeException {
-    public StudyRoomNotFoundException(String message) {
-        super(message);
+
+    public StudyRoomNotFoundException() {
+        super(showNoStudyRoomMessage());
     }
 }

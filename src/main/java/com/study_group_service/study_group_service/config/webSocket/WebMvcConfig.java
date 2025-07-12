2c4ab11a5984f43ed2f9f3d3a8957d3508941726
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-07-12 21:30:13 +0900

    refactor: 패키지 위치 변경

--- src/main/java/com/study_group_service/study_group_service/config/webSocket/WebMvcConfig.java
+++ src/main/java/com/study_group_service/study_group_service/config/webSocket/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.study_group_service.study_group_service.config;
+package com.study_group_service.study_group_service.config.webSocket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;

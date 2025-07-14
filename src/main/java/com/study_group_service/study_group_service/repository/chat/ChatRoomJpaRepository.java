830d8f2a97626702b8e5d931dd8f5c8f3ab83c2e
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-07-14 19:20:40 +0900

    refactor: 불 필요한 공백 제거

--- src/main/java/com/study_group_service/study_group_service/repository/chat/ChatRoomJpaRepository.java
+++ src/main/java/com/study_group_service/study_group_service/repository/chat/ChatRoomJpaRepository.java
@@ -4,5 +4,4 @@ import com.study_group_service.study_group_service.entity.chat.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomJpaRepository extends JpaRepository<ChatRoom, Long> {
-
 }

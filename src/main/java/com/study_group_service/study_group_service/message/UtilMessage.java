17a8aac893785f562933bf346df772de73e9a595
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-07-14 19:21:45 +0900

    refactor: 불 필요한 주석 삭제

--- src/main/java/com/study_group_service/study_group_service/message/UtilMessage.java
+++ src/main/java/com/study_group_service/study_group_service/message/UtilMessage.java
@@ -7,9 +7,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class UtilMessage {
 
-    // 유틸 클래스라 메서드를 static 처리함.
-    // 에러 클래스는 혹시 몰라 메서드 static 처리를 안함.
-
     private static final String ROOM_ID = "roomId";
     private static final String CHAT_ID = "chatId";
     private static final String SEND = "수신 : {}";

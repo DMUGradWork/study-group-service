abf9c18dc459dd085c776434fea393cd5a610080
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-07-14 19:21:03 +0900

    refactor: 불 필요한 공백 제거

--- src/main/java/com/study_group_service/study_group_service/exception/study/StudyRoomNotFoundException.java
+++ src/main/java/com/study_group_service/study_group_service/exception/study/StudyRoomNotFoundException.java
@@ -3,7 +3,6 @@ package com.study_group_service.study_group_service.exception.study;
 import static com.study_group_service.study_group_service.message.ErrorMessage.showNoStudyRoomMessage;
 
 public class StudyRoomNotFoundException extends RuntimeException {
-
     public StudyRoomNotFoundException() {
         super(showNoStudyRoomMessage());
     }

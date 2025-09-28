81cd2516e8cebc9233210e1922a477151e336635
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-09-29 06:14:01 +0900

    feat: UUID 추가로 인한 로직 생성

--- src/main/java/com/study_group_service/study_group_service/repository/community/CommunityCommentRepository.java
+++ src/main/java/com/study_group_service/study_group_service/repository/community/CommunityCommentRepository.java
@@ -4,8 +4,11 @@ import com.study_group_service.study_group_service.entity.community.CommunityCom
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface CommunityCommentRepository extends JpaRepository<CommunityComment, Long> {
     List<CommunityComment> findByPostId(Long postId);
     int countByPostId(Long postId);
+    Optional<CommunityComment> findByUuid(UUID uuid);
 } 

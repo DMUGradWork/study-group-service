da814e053176238d7f7a9c657fee0e4f3692b452
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-09-29 05:39:34 +0900

    feat: 내부 LongID 외에 외부로 보낼 UUID 추가

--- src/main/java/com/study_group_service/study_group_service/service/user/UserService.java
+++ src/main/java/com/study_group_service/study_group_service/service/user/UserService.java
@@ -3,15 +3,21 @@ package com.study_group_service.study_group_service.service.user;
 import com.study_group_service.study_group_service.dto.user.UserDTO;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface UserService {
     List<UserDTO> getUsers();
     UserDTO getUserById(Long id);
+    UserDTO getUserByUuid(UUID uuid);
     UserDTO getUserByEmail(String email);
     UserDTO setUsers(UserDTO userDTO);
     void deleteUser(Long id);
+    void deleteUserByUuid(UUID uuid);
     void updateUserRoleToAdmin(String email);
     void checkAttendance(Long userId);
+    void checkAttendanceByUuid(UUID userUuid);
     void joinRoom(Long userId);
+    void joinRoomByUuid(UUID userUuid);
     void leaveRoom(Long userId);
+    void leaveRoomByUuid(UUID userUuid);
 }
